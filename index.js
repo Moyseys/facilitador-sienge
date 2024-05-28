b0b800e5f4b04965877c40a35c073882150bfa5f
Moyseys Ferreira Veroni <moyseys.veroni@mottasul.com.br>
2024-05-28 16:58:12 -0300

    Get creditor in csv

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const key = process.env.apiKey;
 const user = process.env.apiUser;
 
 const api = new Api(url, key, user);
-const csv = new CSV('cadastro credores.csv');
+const csv = new CSV('cadastro credores TRUE.csv');
 const file = new File()
 
 // api.getCreditor('', '', '', 10).then(async (result) => {
@@ -19,10 +19,26 @@ const file = new File()
 //     file.writeFile('teste.json', data)
 // });
 
-csv.readRow(1002).then((result) => {
-    console.log(result);
+// async function findAndFixCreditors(){
+//     let nextCreditor = await csv.findNextCreditor()
+    
+//     for(let i = 0; i < 3; i++){
+//         if(!nextCreditor) {
+//             console.log("Todos os credores foram corrigidos!!!!!!")
+//             break
+//         }
+//         const { Credor, Documento, Lancamento, Telefone, CORRIGIDO } = nextCreditor
+//         //Corrigir
 
-   
-}).catch((error) => {
-    console.error('Error reading row:', error);
-});
+//         //Alterar status no CSV
+//         await csv.editCell(1, 1, 'TRUE')
+//         //Salvar Json
+        
+//         nextCreditor = await csv.findNextCreditor()
+//     }
+// }
+
+
+csv.editCell('credores.csv', 1, 'CORRIGIDO', 'TRUE')
+    .then(() => console.log('Cell updated successfully'))
+    .catch(err => console.error('Error updating cell:', err));
--- services/Csv.js
+++ services/Csv.js
@@ -1,5 +1,7 @@
-import fs from 'fs'
+import fs, { write } from 'fs'
 import csv from 'csv-parser'
+import { createObjectCsvWriter } from  'csv-writer'
+import { Writable } from 'stream'
 
 export default class CSV {
     constructor(filePath){
@@ -39,5 +41,73 @@ export default class CSV {
         } catch (error) {
             console.error(error)
         }
+    }
+
+    findNextCreditor() {
+        try {
+            return new Promise((resolve, reject) => {
+                let countRow = 0
+
+                const readStream = fs.createReadStream(this.filePath)
+                    .pipe(csv())
+                    .on('data', (data) => {
+                        countRow++
+                        const entries = Object.entries(data)
+                        
+                        const colunm = (entries[0][0]).split(";")
+                        const values = (entries[0][1]).split(';')
+
+                        const correted = values[values.length - 1] === 'TRUE' 
+        
+                        if(!correted){
+                            const valueFormated = colunm.reduce((ac, vl, i) => {
+                                ac[vl] = values[i]
+                                return ac
+                            }, {})
+                            resolve(valueFormated)
+                            readStream.destroy()
+                        }
+                    })
+                    .on('end', () => {
+                        resolve(null)
+                    })
+                    .on('error', (error) => {
+                        reject(new Error(error))
+                    });
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    editCell(row, colunm, newValue){
+        return new Promise((resolve, reject) => {
+            let records = []
+            let header = []
+            const readStream = fs.createReadStream(this.filePath)
+                .pipe(csv())
+                .on('data', (row) => {
+                    console.log(row);
+                    header = Object.keys(row)
+                    records.push(row)
+                })
+                .on('end', () => {
+                    const csvWriter = createObjectCsvWriter({
+                        path: 'path/to/file.csv',
+                        header: header.map((vl) => {
+                            return {
+                                id: vl,
+                                title: vl
+                            }
+                        })
+                    });
+                    //console.log(header);
+                    // csvWriter.writeRecords(records)
+                    // console.log('CSV file successfully processed');
+                })
+                .on('error', (err) => {
+                    console.error('Pipeline failed.', err);
+                });
+        })
     }
 }
