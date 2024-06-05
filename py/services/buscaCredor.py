c0c46d5f2d6094cb3731466d4abfc6e6b9507dfd
Moyseys Ferreira Veroni <moyseys.veroni@mottasul.com.br>
2024-06-05 14:34:34 -0300

    Opitimization to work on multiples browsers

--- py/services/buscaCredor.py
+++ py/services/buscaCredor.py
@@ -1,25 +1,31 @@
 import pyautogui
 import os
+import time
+
+def localizar_e_clicar(imagem, descricao):
+    localizacao = pyautogui.locateCenterOnScreen(imagem, confidence=0.8)
+    pyautogui.click(localizacao)
+    time.sleep(1)
+
 
 def buscarCredor(razaoSocial):
     try:
-        icon_input_razao_social = os.path.join(os.getcwd(), 'py', 'static', 'icons', 'inputRazaoSocial.png')
-        icon_input_razao_social = icon_input_razao_social.replace('\\', '/')
-        print(icon_input_razao_social)
+        base_path = os.path.join(os.getcwd(), 'py', 'static', 'icons').replace('\\', '/')
+        
+        icon_input_razao_social = os.path.join(base_path, 'inputRazaoSocial.png')
+        icon_btn_consultar = os.path.join(base_path, 'btnConsultar.png')
+        icon_btn_pensil = os.path.join(base_path, 'btnPensil.png')
 
 
-        cord_input_razao_social = pyautogui.locateOnScreen("C:/Users/MoyseysFerreiraVeron/Desktop/facilitador-sienge/py/static/icons/inputRazaoSocial.png")
-        pyautogui.moveTo(cord_input_razao_social)
-        pyautogui.click()
+        localizar_e_clicar(icon_input_razao_social, 'inputRazaoSocial')
         pyautogui.write(razaoSocial)
+        
+        localizar_e_clicar(icon_btn_consultar, 'btnConsultar')
+        localizar_e_clicar(icon_btn_pensil, 'btnPensil')
 
-        icon_btn_consultar = os.path.join(os.getcwd(), 'py', 'static', 'icons', 'btnConsultar.png')
-        icon_btn_consultar = icon_input_razao_social.replace('\\', '/')
-        cord_btn_consultar = pyautogui.locateOnScreen(icon_btn_consultar)
-        pyautogui.moveTo(cord_btn_consultar)
-        pyautogui.click()
-
-    except pyautogui.ImageNotFoundException:
-        print("A imagem não pôde ser encontrada na tela.")
+        return
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
+
+time.sleep(3)
+buscarCredor("Softplan")
